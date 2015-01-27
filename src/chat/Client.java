11ab25f456bbe345b0165e6b3c41f2d2c304e617
GyselAnimatioon <GyselAnimatioon@WIN-VN7MNVHFP5G>
2015-01-27 06:58:59 -0600

    icon geadded

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -3,6 +3,8 @@ package chat;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -19,6 +21,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
+import sun.awt.image.ToolkitImage;
 
 public class Client {
 
@@ -41,6 +44,7 @@ public class Client {
     public void createGUI() {
         clientFrame = new JFrame("Mein Chat");
         clientFrame.setSize(800, 600);
+        clientFrame.setIconImage(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("icon.png")));
         clientPanel = new JPanel();
         textArea_Messages = new JTextArea();
         textArea_Messages.setEditable(false);
