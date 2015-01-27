31457b9b3067b58f7e1f31c6560ac90f7ff72244
GyselAnimatioon <GyselAnimatioon@WIN-VN7MNVHFP5G>
2015-01-27 07:48:41 -0600

    Username geadded. Name Handler geändert. login message geändert. name, login geadded.

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -14,6 +14,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -26,33 +28,71 @@ import sun.awt.image.ToolkitImage;
 public class Client {
 
     JFrame clientFrame;
+    JFrame loginFrame;
     JPanel clientPanel;
+    JPanel loginPanel;
     JTextArea textArea_Messages;
     JTextField textField_ClientMessage;
+    JTextField loginUsername;
     JButton button_SendMessage;
+    JButton loginSubmit;
     JTextField textField_Username;
     JScrollPane scrollPane_Messages;
     Socket client;
     PrintWriter writer;
     BufferedReader reader;
+    int login = 0;
+    String name = "Unnamed";
 
     public static void main(String[] args) {
         Client c = new Client();
+        c.loginGUI(c);
+    }
+
+    public void loginGUI(Client c) {
         c.createGUI();
+        loginFrame = new JFrame("Login");
+        loginFrame.setSize(400, 100);
+        loginFrame.setLocationRelativeTo(null);
+        loginFrame.setResizable(false);
+        loginPanel = new JPanel();
+        loginUsername = new JTextField(30);
+        loginUsername.setText(System.getProperty("user.name"));
+        loginUsername.setEditable(true);
+        loginUsername.addKeyListener(new SendPressEnterListener());
+        loginSubmit = new JButton("Starten");
+        SendButtonListener start = new SendButtonListener();
+        loginSubmit.addActionListener(start);
+
+        loginPanel.add(loginUsername);
+        loginPanel.add(loginSubmit);
+
+        loginFrame.getContentPane().add(BorderLayout.CENTER, loginPanel);
+        loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginFrame.setVisible(true);
+        loginFrame.pack();
     }
 
     public void createGUI() {
         clientFrame = new JFrame("Mein Chat");
         clientFrame.setSize(800, 600);
-        clientFrame.setIconImage(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("icon.png")));
+        clientFrame.setLocationRelativeTo(null);
+        clientFrame.setResizable(false);
+        //clientFrame.setIconImage(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("icon.png")));
+
         clientPanel = new JPanel();
+
         textArea_Messages = new JTextArea();
         textArea_Messages.setEditable(false);
+
         textField_ClientMessage = new JTextField(38);
         textField_ClientMessage.addKeyListener(new SendPressEnterListener());
+
         button_SendMessage = new JButton("Send");
         button_SendMessage.addActionListener(new SendButtonListener());
+
         textField_Username = new JTextField(10);
+
         scrollPane_Messages = new JScrollPane(textArea_Messages);
         scrollPane_Messages.setPreferredSize(new Dimension(700, 500));
         scrollPane_Messages.setMinimumSize(new Dimension(700, 500));
@@ -70,6 +110,7 @@ public class Client {
         clientFrame.getContentPane().add(BorderLayout.CENTER, clientPanel);
         clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         clientFrame.setVisible(true);
+        //clientFrame.pack();
     }
 
     public boolean connectToServer() {
@@ -78,7 +119,7 @@ public class Client {
             reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
             writer = new PrintWriter(client.getOutputStream());
             appendTextMessages("Netzwerkverbindung hergestellt");
-            writer.println("Neuer User gejoint");
+            writer.println(System.getProperty("user.name") + " hat den Chatraum betreten.");
             writer.flush();
             return true;
         } catch (Exception e) {
@@ -88,8 +129,13 @@ public class Client {
     }
 
     public void sendMessageToServer() {
-        writer.println(textField_Username.getText() + ": " + textField_ClientMessage.getText());
-        writer.flush();
+        if (!textField_Username.getText().equals("")) {
+            this.name = textField_Username.getText();
+        }
+        if (!textField_ClientMessage.getText().equals("")) {
+            writer.println(name + ": " + textField_ClientMessage.getText());
+            writer.flush();
+        }
         textField_ClientMessage.setText("");
         textField_ClientMessage.requestFocus();
     }
@@ -103,7 +149,16 @@ public class Client {
         @Override
         public void keyPressed(KeyEvent arg0) {
             if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
-                sendMessageToServer();
+                if (login == 0) {
+                    if (!loginUsername.getText().equals("")) {
+                        name = loginUsername.getText();
+                    }
+                    loginFrame.dispose();
+                    login = 1;
+                    textField_ClientMessage.requestFocus();
+                } else {
+                    sendMessageToServer();
+                }
             }
         }
 
@@ -120,7 +175,16 @@ public class Client {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            sendMessageToServer();
+            if (login == 0) {
+                if (!loginUsername.getText().equals("")) {
+                    name = loginUsername.getText();
+                }
+                loginFrame.dispose();
+                login = 1;
+                textField_ClientMessage.requestFocus();
+            } else {
+                sendMessageToServer();
+            }
         }
     }
 
