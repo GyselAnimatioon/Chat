c90c9e2dbef1d46b2fe4dd860f92764045a9304e
GyselAnimatioon <GyselAnimatioon@WIN-VN7MNVHFP5G>
2015-01-27 00:48:45 -0600

    finish code

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,36 +1,100 @@
+// Package muss natürlich angepasst werden
 package chat;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Server {
 
+    ServerSocket server;
+    ArrayList<PrintWriter> list_clientWriter;
+    final int LEVEL_ERROR = 1;
+    final int LEVEL_NORMAL = 0;
+
     public static void main(String[] args) {
+        Server s = new Server();
+        if (s.runServer()) {
+            s.listenToClients();
+        } else {
+            // Do nothing
+        }
+    }
+
+    public class ClientHandler implements Runnable {
+
+        Socket client;
+        BufferedReader reader;
+
+        public ClientHandler(Socket client) {
+            try {
+                this.client = client;
+                reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            String nachricht;
+            try {
+                while ((nachricht = reader.readLine()) != null) {
+                    appendTextToConsole("Vom Client: \n" + nachricht, LEVEL_NORMAL);
+                    sendToAllClients(nachricht);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void listenToClients() {
         while (true) {
             try {
-                System.out.println("Server wird gestartet...");
-                ServerSocket server = new ServerSocket(5678);
-                System.out.println("Server wurde gestartet!");
-
-                System.out.println("Warte auf Client...");
                 Socket client = server.accept();
-                System.out.println("Client verbunden!");
-
-                Thread t1 = new Thread(new Handler(client));
-                t1.start();
-                
-            } catch (IOException ex) {
-                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                PrintWriter writer = new PrintWriter(client.getOutputStream());
+                list_clientWriter.add(writer);
+                Thread clientThread = new Thread(new ClientHandler(client));
+                clientThread.start();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+        }
+    }
 
+    public boolean runServer() {
+        try {
+            server = new ServerSocket(5555);
+            appendTextToConsole("Server wurde gestartet!", LEVEL_ERROR);
+            list_clientWriter = new ArrayList<PrintWriter>();
+            return true;
+        } catch (IOException e) {
+            appendTextToConsole("Server konnte nicht gestartet werden!", LEVEL_ERROR);
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void appendTextToConsole(String message, int level) {
+        if (level == LEVEL_ERROR) {
+            System.err.println(message + "\n");
+        } else {
+            System.out.println(message + "\n");
+        }
+    }
+
+    public void sendToAllClients(String message) {
+        Iterator it = list_clientWriter.iterator();
+        while (it.hasNext()) {
+            PrintWriter writer = (PrintWriter) it.next();
+            writer.println(message);
+            writer.flush();
         }
     }
 }
